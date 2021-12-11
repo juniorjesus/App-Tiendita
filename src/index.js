660cd7d235ece86c210226cee0376351ea4de812
Jrjesus18 <juniorlopez3@live.com>
2021-12-11 13:38:18 -0500

    update

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+<<<<<<< HEAD
+import AppRouter from './routers/AppRouter';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppRouter />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+=======
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+>>>>>>> 57c9b228c710fc1b54d5f64a422733c4831b1bd2
